fc264387e552f91d594ae15505a8c508d9cca073
unknown <adrianji1945@gmail.com>
2023-01-28 10:09:32 +0700

    add skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,6 +34,10 @@ export default function Home() {
           <div className="mt-8 pt-2 px-4">
             <div className="w-full bg-slate-300 h-56 animate-pulse rounded-lg"></div>
           </div>
+          <div className="w-full flex justify-between px-4 mt-6">
+            <div className="bg-slate-300  animate-pulse w-4/12 h-8 rounded-full"></div>
+            <div className="bg-slate-300  animate-pulse w-2/12 h-8 rounded-full"></div>
+          </div>
         </>
       ) : (
         <>
